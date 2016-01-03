8d3eea61e551678b8a93084cfbcce624e1720a9a
admiral-f <admiral-f@yandex.ru>
2016-01-03 13:32:04 +0300

    change pony code

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
     if @contact.save
       Pony.mail(
           :to => "admiral-f@yandex.ru",
-          :from => @contact.email,
+          :from => "admiral-f@yandex.ru",
           :subject => @contact.name + " has contacted you",
           :body => @contact.email + " write message: " + @contact.message,
           :via => :smtp,
