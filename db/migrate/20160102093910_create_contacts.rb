dcbaa531ba66ac43f3d6dbe1784603f17ef40d56
admiral-f <admiral-f@yandex.ru>
2016-01-03 11:52:19 +0300

    add columns to database

--- db/migrate/20160102093910_create_contacts.rb
+++ db/migrate/20160102093910_create_contacts.rb
@@ -1,6 +1,9 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
+      t.string :email
+      t.string :name
+      t.text :message
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@
 ActiveRecord::Schema.define(version: 20160102093910) do
 
   create_table "contacts", force: :cascade do |t|
+    t.string   "email"
+    t.string   "name"
+    t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
