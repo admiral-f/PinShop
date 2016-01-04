9a79ed8fe95cd75cd89a17f910f1e75f0536e7f6
admiral-f <admiral-f@yandex.ru>
2016-01-04 22:24:04 +0300

    change image validation

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,6 +3,8 @@ class Pin < ActiveRecord::Base
   validates :price, presence: true
   validates :description, presence: true
   validates :material, presence: true
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" },
+                     default_url: "/images/:style/missing.png",
+                     :keep_old_files => :true
   validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
 end
