c70dde3168abfa491233946e9162b395e3bb90f9
admiral-f <admiral-f@yandex.ru>
2016-01-12 14:52:43 +0300

    input order to orders table

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,28 @@
 class OrdersController < ApplicationController
+  before_filter :authenticate_user!, :only=>[:create, :edit]
+
+  def create 
+    @order=Order.new(order_params)
+    @order.email=current_user.email
+    @order.phone=current_user.phone
+    @order.first_name=current_user.first_name
+    @order.last_name=current_user.last_name
+    if @order.save
+      redirect_to @order
+    else
+      @error="your cart is empty"
+      redirect_to pins_path
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order)
+  end
+
+
 end
