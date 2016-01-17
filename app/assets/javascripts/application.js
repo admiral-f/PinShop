153ed20268d72874db507564a9aa4f0ff24fa446
admiral-f <admiral-f@yandex.ru>
2016-01-17 13:02:46 +0300

    add js function add_order_table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -81,3 +81,62 @@ function cart_get_orders()
 	return orders;
 }
 
+function add_order_table()
+{
+	if (cart_get_number_of_items()==0){
+		var table = document.getElementById("OrdersTable");
+	    var header = table.createTHead();
+	    var row = header.insertRow(0);
+	    var cell1 = row.insertCell(0);
+	    cell1.innerHTML = "<b>Empty cart</b>";
+	} else {
+		var total_price='';
+		var table = document.getElementById("OrdersTable");
+	    var header = table.createTHead();
+	    var row = header.insertRow(0);
+	    var cell1 = row.insertCell(0);
+	    var cell2 = row.insertCell(1);
+	    var cell3 = row.insertCell(2);
+	    var cell4 = row.insertCell(3);
+	    var cell5 = row.insertCell(4);
+	    cell1.innerHTML = "<b>#</b>";
+	    cell2.innerHTML = "<b>Pin</b>";
+	    cell3.innerHTML = "<b>Price</b>";
+	    cell4.innerHTML = "<b>Quantity</b>";
+	    cell5.innerHTML = "<b>Sum Price</b>";
+	    for(var i =0; i < window.localStorage.length; i++)
+		{
+			var key = window.localStorage.key(i);
+			var value = window.localStorage.getItem(key);
+			value=JSON.parse(value);
+			if(key.indexOf('product_')==0)
+			{
+				var row = table.insertRow(i+1);
+				var cell1 = row.insertCell(0);
+	    		var cell2 = row.insertCell(1);
+	    		var cell3 = row.insertCell(2);
+	    		var cell4 = row.insertCell(3);
+	    		var cell5 = row.insertCell(4);
+	    		cell1.innerHTML = '<b>' + (i+1) + '</b>';
+	    		cell2.innerHTML = '<a href="/pins/'+value[0]+'">'+value[1]+'</a>';
+	    		cell3.innerHTML = value[2];
+	    		cell4.innerHTML = value[3];
+	    		cell5.innerHTML = value[2]*value[3];
+	    		total_price=total_price*1+value[2]*value[3];
+
+			}
+		}
+		var row = table.insertRow();
+		var cell1 = row.insertCell(0);
+	    var cell2 = row.insertCell(1);
+	    var cell3 = row.insertCell(2);
+	    var cell4 = row.insertCell(3);
+	    var cell5 = row.insertCell(4);
+	    cell1.innerHTML = "";
+	    cell2.innerHTML = "Total Quantity";
+	    cell3.innerHTML = cart_get_number_of_items();
+	    cell4.innerHTML = "Total Price";
+	    cell5.innerHTML = total_price;
+	}
+} 
+
