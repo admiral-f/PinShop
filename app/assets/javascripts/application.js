b42fa66bd4a0a9c061b65df5ee70e946f28e5f4e
admiral-f <admiral-f@yandex.ru>
2016-01-17 13:31:23 +0300

    add change quantity js function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -140,3 +140,22 @@ function add_order_table()
 	}
 } 
 
+function decrease_quantity(id)
+{
+	var key = 'product_' + id;
+	var x=window.localStorage.getItem(key);
+	x=JSON.parse(x);
+	x[3]=x[3]*1-1;
+	window.localStorage.setItem(key, JSON.stringify(x));
+	location.reload();
+}
+
+function increase_quantity(id)
+{
+	var key = 'product_' + id;
+	var x=window.localStorage.getItem(key);
+	x=JSON.parse(x);
+	x[3]=x[3]*1+1;
+	window.localStorage.setItem(key, JSON.stringify(x));
+	location.reload();
+}
