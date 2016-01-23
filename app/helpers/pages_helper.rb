111560e56fa39b43460524b3aabddfdc24832c06
admiral-f <admiral-f@yandex.ru>
2016-01-23 20:03:29 +0300

    add devise helpers to display edit_path on profile view

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,17 @@
 module PagesHelper
+  def resource_name
+    :user
+  end
+
+    def resource
+      @resource ||= current_user
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def resource_class
+    devise_mapping.to
+  end
 end
