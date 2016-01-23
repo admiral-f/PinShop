b5568a3aeee2221e074be12bebdc8d706f094f01
admiral-f <admiral-f@yandex.ru>
2016-01-23 17:26:53 +0300

    add profile function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,15 @@
 class PagesController < ApplicationController
-  before_filter :authenticate_user!, :only=>[:cart]
+  before_filter :authenticate_user!, :only=>[:cart, :profile]
   
   def index
   end
 
   def cart
     @pins=Pin.all
+  end
+
+  def profile
+    @orders=Order.where(email: current_user)
   end
 
 end
