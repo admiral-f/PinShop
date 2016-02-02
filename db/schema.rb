dc8f0a33c43f674f67cbe115046e5f102db5aee1
admiral-f <admiral-f@yandex.ru>
2016-02-02 21:08:09 +0300

    add columns to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120172059) do
+ActiveRecord::Schema.define(version: 20160202170519) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 20160120172059) do
     t.string   "phone"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
