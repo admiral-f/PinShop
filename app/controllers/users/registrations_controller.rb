c102e79ee07a7fa1f542f416a7a9d961044385c9
admiral-f <admiral-f@yandex.ru>
2016-02-05 21:15:56 +0300

    update user info without password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 # before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
+  protected
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
   # GET /resource/sign_up
   # def new
   #   super
