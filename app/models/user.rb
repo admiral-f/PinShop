29226c72d0cea84820ea4b67af5dfcf59b177547
admiral-f <admiral-f@yandex.ru>
2016-02-05 21:16:37 +0300

    input user info from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
         user.password = Devise.friendly_token[0,20]
         user.first_name = auth.info.first_name
         user.last_name = auth.info.last_name
+        user.username = auth.info.name
       end
   end
 
