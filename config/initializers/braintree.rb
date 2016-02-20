c07cb6a07c13d4ccc24c71ec010c081b8113219f
admiral-f <admiral-f@yandex.ru>
2016-02-20 18:48:14 +0300

    add braintree initializer

---
+++ config/initializers/braintree.rb
@@ -0,0 +1,5 @@
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.logger = Logger.new('log/braintree.log')
+Braintree::Configuration.merchant_id = 'pynw5bn48gsv6rgb'
+Braintree::Configuration.public_key = 'mmsz7cbkh7g9nsn2'
+Braintree::Configuration.private_key = '584a1eea9194ea48a9811e5b05c22dbc'
