772caec97936967b297d547875c06bb427b85703
admiral-f <admiral-f@yandex.ru>
2016-02-22 13:20:08 +0300

    Generate a client token

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,10 @@
 class TransactionsController < ApplicationController
   def new
+    gon.client_token = generate_client_token
+  end
+
+  private
+  def generate_client_token
+    Braintree::ClientToken.generate
   end
 end
